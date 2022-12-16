4cfff7afa5d7f36322e6a89589d71c82239a8cc8
Marina <114664502+marina2523@users.noreply.github.com>
2022-12-16 16:14:26 +0300

    Add files via upload

---
+++ Задание 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static string Reverse(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return s;
+        }
+        return Reverse(s.Substring(1)) + s[0].ToString();
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        var inputText = Console.ReadLine();
+        Console.WriteLine("Перевернутая строка: " + Reverse(inputText));
+        Console.ReadLine();
+    }
+}
